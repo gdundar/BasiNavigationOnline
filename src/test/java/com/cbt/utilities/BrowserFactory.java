27f6468c2d15699d779ba09a20160c078b4477e2
cybertekschool <github@cybertekschool.com>
2019-10-28 14:35:16 -0400

    my first commit

---
+++ src/test/java/com/cbt/utilities/BrowserFactory.java
@@ -0,0 +1,15 @@
+package com.cbt.utilities;
+
+import org.openqa.selenium.WebDriver;
+
+public class BrowserFactory {
+
+    public static WebDriver getDriver(String browser){
+        //for demo
+        //I didn't put any code yet
+        //to check which os is currently used:
+        //for mac
+        String os = System.getProperty("os.name");
+       return null;
+    }
+}
